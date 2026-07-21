0d30c64825e079652db34cbec73286e3378f010c
sandikaleproject-art <sandikaleproject@gmail.com>
2026-07-21 10:09:00 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+// DATA SIMULASI (nanti ini dari database)
+let produk = 10;
+let customer = 5;
+let transaksi = 20;
+let penjualan = 1500000;
 
+// TAMPILKAN KE DASHBOARD
+document.getElementById("totalProduk").innerText = produk;
+document.getElementById("totalCustomer").innerText = customer;
+document.getElementById("totalTransaksi").innerText = transaksi;
+document.getElementById("totalPenjualan").innerText = "Rp " + penjualan.toLocaleString();
